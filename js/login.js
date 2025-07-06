7cb7f858c44cc4745b8e1456b95039b097251578
Kwon <kdg5436@gmail.com>
2025-07-06 23:40:14 +0900

    feat: redirect url 수정

--- js/login.js
+++ js/login.js
@@ -5,18 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
       const clientId =
         "11466001540-d1mrajklvc7grgsa2lpgdja3h6ltcdma.apps.googleusercontent.com";
       const redirectUri = encodeURIComponent(
-        "https://focuscoach.click/oauth/callback.html"
+        "https://vercel-test-mu-dusky.vercel.app/oauth/callback.html"
       );
       const scope = encodeURIComponent("email profile");
-      const state = "poco-login";
-
       const oauthUrl =
         `https://accounts.google.com/o/oauth2/v2/auth` +
         `?response_type=code` +
         `&client_id=${clientId}` +
         `&redirect_uri=${redirectUri}` +
         `&scope=${scope}` +
-        `&state=${state}` +
         `&access_type=offline` +
         `&prompt=consent`;
 
